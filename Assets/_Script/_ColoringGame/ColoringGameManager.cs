099556717703afb8ba7e1e8243410d1b3c608284
Vinh Truong <vinhphuc.truong@gmail.com>
2013-10-16 17:15:23 +0300

    Measure different region proportions

--- Assets/_Script/_ColoringGame/ColoringGameManager.cs
+++ Assets/_Script/_ColoringGame/ColoringGameManager.cs
@@ -2,12 +2,27 @@
 using System.Collections;
 
 public class ColoringGameManager : GameManager {
-
+	
+	// Defined regions
+	public Rect chosenCharRegion;		// 20,20,160,160
+	public Rect pictureSelectRegion;	// 20,200,160,380
+	public Rect pictureRegion;			// 200,20,560,560
+	public Rect toolbarRegion;			// 780,20,160,560
+	
 	public override void Start () {
-		Debug.Log("vinh was here");
+		base.Start();
+		
+		SetGameState(GameState.Running);
 	}
 	
 	void Update () {
 	
+	}
+	
+	void OnGUI () {
+		GUI.Box(chosenCharRegion, "chosenChar");
+		GUI.Box(pictureSelectRegion, "pictureSelect");
+		GUI.Box(pictureRegion, "picture");
+		GUI.Box(toolbarRegion, "toolbar");
 	}
 }
