198bd4681d630c995829a3ff2c0b57bc5384a18d
Vinh Truong <vinh.truong@metropolia.fi>
2013-10-16 17:40:31 +0300

    Change picture region to Texture2D. Small test

--- Assets/_Script/_ColoringGame/ColoringGameManager.cs
+++ Assets/_Script/_ColoringGame/ColoringGameManager.cs
@@ -9,10 +9,23 @@ public class ColoringGameManager : GameManager {
 	public Rect pictureRegion;			// 200,20,560,560
 	public Rect toolbarRegion;			// 780,20,160,560
 	
+	private Texture2D picture;
+	
 	public override void Start () {
 		base.Start();
 		
 		SetGameState(GameState.Running);
+		
+		picture = new Texture2D(560, 560);
+		for (int x = 0; x < 560; x++) {
+			for (int y = 0; y < 560; y++) {
+				if (x % 10 == 0 || y % 10 == 0)
+					picture.SetPixel(x, y, new Color(1.0f, 0.5f, 0.0f, 1.0f));
+				else
+					picture.SetPixel(x, y, Color.clear);
+			}
+		}
+		picture.Apply();
 	}
 	
 	void Update () {
@@ -22,7 +35,7 @@ public class ColoringGameManager : GameManager {
 	void OnGUI () {
 		GUI.Box(chosenCharRegion, "chosenChar");
 		GUI.Box(pictureSelectRegion, "pictureSelect");
-		GUI.Box(pictureRegion, "picture");
+		GUI.DrawTexture(pictureRegion, picture, ScaleMode.StretchToFill, true);
 		GUI.Box(toolbarRegion, "toolbar");
 	}
 }
