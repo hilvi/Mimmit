531f9472054b27be01096391fb7a3ea56c879f9e
Lucas <lucas.cosson@metropolia.com>
2013-10-18 15:17:46 +0300

    Added functionalities for horse

--- Assets/_Script/_HorseGame/HorseCharacterController.cs
+++ Assets/_Script/_HorseGame/HorseCharacterController.cs
@@ -5,26 +5,54 @@ using System.Collections;
 public class HorseCharacterController : MonoBehaviour {
 	
 	public float gravity = 20;
-	public float speed = 5;
+	public float runningSpeed = 5;
 	public float jumpSpeed = 10;
+	public float mudSpeed = 2;
 	
 	private CharacterController _controller;
 	private Vector3 _movement;
+	private float _currentSpeed;
+	public float currentSpeed{
+		set{_currentSpeed = value;}
+	}
 	
 	void Start () {
 		_controller = GetComponent<CharacterController>();
-		_movement.x = speed;
+		_currentSpeed = runningSpeed;
+		_movement.x = _currentSpeed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if(_controller.isGrounded) 
 		{
 			if(Input.GetButtonDown("Jump")) {
 				_movement.y = jumpSpeed;
 			}
 		}else 
-		_movement.y -= gravity * Time.deltaTime;
+			_movement.y -= gravity * Time.deltaTime;
+		
+		_movement.x = _currentSpeed;
 		_controller.Move(_movement*Time.deltaTime);
+	}
+	
+	/// <summary>
+	/// Enters the mud configuration.
+	/// When stepping in mud, this is called and set the horse as mud configuration.
+	/// Speed is slowed down and the horse is set halfway into the ground
+	/// </summary>
+	public void EnterMudConfiguration()
+	{
+		currentSpeed = mudSpeed;
+	}
+	
+	/// <summary>
+	/// Exits the mud configuration.
+	/// When stepping out of the mud, the horse gets initla speed and position
+	/// </summary>
+	public void ExitMudConfiguration()
+	{
+		currentSpeed = runningSpeed;
 	}
 }
