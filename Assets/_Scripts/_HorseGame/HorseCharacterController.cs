4f96c5fd09ed92206a86aa01d47176a09514caca
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-10-24 10:25:35 +0300

    Add dummy prefab for rocks and sidestepping for dodging them

--- Assets/_Scripts/_HorseGame/HorseCharacterController.cs
+++ Assets/_Scripts/_HorseGame/HorseCharacterController.cs
@@ -8,21 +8,30 @@ public class HorseCharacterController : MonoBehaviour {
 	public float runningSpeed = 5;
 	public float jumpSpeed = 10;
 	public float mudSpeed = 2;
+	public Vector3 sideStep = new Vector3(0, -0.3f, -0.5f);
 	public Animator2D anim;
 	
 	private CharacterController _controller;
 	private Vector3 _movement;
 	private float _currentSpeed;
+	Transform _plane;
 	
 	void Start () 
 	{
 		_controller = GetComponent<CharacterController>();
+		_plane = transform.Find("Plane");
 		_currentSpeed = runningSpeed;
 		_movement.x = _currentSpeed;
+		_controller.radius = 0.1f;
 	}
 	
 	void Update () 
 	{
+		if(Input.GetButtonDown("Fire1"))
+			SideStep();
+		if(Input.GetButtonUp ("Fire1"))
+			SideStep ();
+		
 		if(_controller.isGrounded) 
 		{ 
 			if(_controller.velocity.x != 0)
@@ -65,5 +74,16 @@ public class HorseCharacterController : MonoBehaviour {
 	public void SetSpeed(float speed)
 	{
 		_currentSpeed = speed;
+	}
+	public void SideStep() {
+		Vector3 __tmp = _plane.position;
+		__tmp.y += sideStep.y;
+		_plane.position = __tmp;
+		
+		__tmp = transform.position;
+		__tmp.z += sideStep.z;
+		transform.position = __tmp;
+		
+		sideStep *= -1;
 	}
 }
