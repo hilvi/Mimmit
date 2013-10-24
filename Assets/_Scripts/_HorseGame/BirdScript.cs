b6ae7abe70232b5677453e5c2b0d89c57ca825df
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-10-24 09:18:21 +0300

    Animate bird

--- Assets/_Scripts/_HorseGame/BirdScript.cs
+++ Assets/_Scripts/_HorseGame/BirdScript.cs
@@ -5,6 +5,7 @@ public class BirdScript : MonoBehaviour {
 
 	#region MEMBERS
 	public float speed;
+	private Animator2D _anim;
 	private Transform _transform;
 	private Vector3 _translation;
 	#endregion
@@ -15,12 +16,13 @@ public class BirdScript : MonoBehaviour {
 	{
 		_transform = GetComponent<Transform>();
 		_translation = new Vector3(speed,0,0);
+		_anim = GetComponentInChildren<Animator2D>();
 	}
 
 	void Update () 
 	{	
 		_transform.Translate(_translation * Time.deltaTime);
-		
+		_anim.PlayAnimation("Bird");
 	}
 	void OnTriggerEnter(Collider col)
 	{
