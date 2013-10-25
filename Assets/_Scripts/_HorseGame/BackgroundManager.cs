20bf13785ef4b88e15aa8b06dacb258e04bcf996
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-10-25 19:13:32 +0300

    Use some brawn to solve the problem

--- Assets/_Scripts/_HorseGame/BackgroundManager.cs
+++ Assets/_Scripts/_HorseGame/BackgroundManager.cs
@@ -2,30 +2,35 @@
 using System.Collections;
 
 public class BackgroundManager : MonoBehaviour {
-	public Texture2D texture;
-	public int tiles;
+	public Texture2D[] textures;
 
 	// Use this for initialization
 	void Start () {
 		Shader __diffuse = Shader.Find("Diffuse");
 		
 		Material __mat = new Material(__diffuse);
-		__mat.mainTexture = texture;
-		__mat.mainTextureScale = new Vector2(tiles, 1);
-		__mat.mainTexture.wrapMode = TextureWrapMode.Repeat;
+		__mat.mainTexture = _MergeTextures(textures);
 		renderer.material = __mat;
-		
-		
-		/*__scale = textures.Length;
-		Material[] __mats = new Material[__scale];
-		for(int i = 0; i < __scale; i++) {
-			Material __mat = new Material(Shader.Find("Diffuse"));
-			__mat.mainTexture = textures[i];
-			__mat.mainTextureScale = new Vector2(__scale, 1);
-			__mat.mainTextureOffset = new Vector2(i, 0);
-			__mat.mainTexture.wrapMode = TextureWrapMode.Clamp;
-			__mats[i] = __mat;
+	}
+	
+	private Texture2D _MergeTextures(Texture2D[] __textures) {
+		int __x = 0, __y = 0;
+		foreach(Texture2D texture in __textures) {
+			__x += texture.width;
+			__y = Mathf.Max(__y, texture.height);
 		}
-		renderer.materials = __mats;*/
+		Texture2D __texture = new Texture2D(__x, __y);
+		
+		int __offset = 0;
+		foreach(Texture2D texture in __textures) {
+			for(int i = 0; i < texture.height; i++) {
+				for(int j = 0; j < texture.width; j++) {
+					__texture.SetPixel(j+__offset, i, texture.GetPixel(j,i));
+				}
+			}
+			__offset += texture.width;
+		}
+		__texture.Apply();
+		return __texture;
 	}
 }
