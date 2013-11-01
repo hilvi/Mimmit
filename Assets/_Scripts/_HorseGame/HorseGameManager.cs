a3628e5b9529af3158547350ff96af7ae5caae1f
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-11-01 13:35:40 +0200

    Add progressbar to horsegame

--- Assets/_Scripts/_HorseGame/HorseGameManager.cs
+++ Assets/_Scripts/_HorseGame/HorseGameManager.cs
@@ -8,14 +8,32 @@ public class HorseGameManager : GameManager {
 	private bool _playerFinished = false;
 	private bool _birdFinished = false;
 	private bool _gameOver;
-	private Rect _boxRect;
 	private CharacterWidgetScript _characterWidget;
+	
+	private Rect _progressBar;
+	private Rect _progressPlayerPos;
+	private Rect _progressBirdPos;
+	private Transform _playerPosition;
+	private Transform _birdPosition;
+	private float _levelLength;
+	private Texture2D _levelTexture;
 	
 	public HorseCharacterController horseScript;
 	public BirdScript birdScript;
 	public AudioClip music;
 	public GameObject musicObject;
+	public float progressBarLength = 500;
+	public Texture2D playerTexture;
+	public Texture2D birdTexture;
+	public float progressCharacterSize = 50;
 	#endregion
+	
+	private float PositionToProgress(Transform pos) {
+		float __currentPos = pos.transform.position.x;
+		__currentPos = __currentPos / _levelLength * progressBarLength;
+		Debug.Log(__currentPos);
+		return _progressBar.x+__currentPos-15;
+	}
 	
 	#region UNITY_METHODS
 	public override void Start () 
@@ -29,9 +47,21 @@ public class HorseGameManager : GameManager {
 		}
 		_characterWidget = GetComponent<CharacterWidgetScript>();
 		SetGameState(GameState.Running);
+		
 		float __width = Screen.width / 2;
-		float __height = Screen.height / 2;
-		_boxRect = new Rect(Screen.width /2  - __width / 2, Screen.height / 2 - __height / 2, __width , __height);
+		Transform __finishPosition = GameObject.Find ("FinishLine").transform;
+		_progressBar = new Rect(0,0, progressBarLength, 25);
+		_progressBar.center = new Vector2(__width, 25);
+		_playerPosition = GameObject.Find("Player").transform;
+		_birdPosition = GameObject.Find ("Bird").transform;
+		
+		_levelLength = __finishPosition.position.x - _playerPosition.position.x;
+		_progressPlayerPos = new Rect(0, 0, progressCharacterSize, progressCharacterSize);
+		_progressPlayerPos.center = new Vector2(0,25);
+		_progressBirdPos = new Rect(0, 0, progressCharacterSize, progressCharacterSize);
+		_progressBirdPos.center = new Vector2(0,25);
+		
+		_levelTexture = GameObject.Find ("Background").GetComponentInChildren<BackgroundManager>().background;
 		
 		gameObject.AddComponent<GUIText>();
 		gameObject.transform.position = new Vector3(0.5f, 0.5f, 0);
@@ -62,6 +92,13 @@ public class HorseGameManager : GameManager {
 		float fps  = 1/Time.deltaTime;
 		GUI.Box (new Rect(0,0,100,50),fps.ToString());
 #endif
+		
+		GUI.DrawTexture(_progressBar, _levelTexture);
+		_progressBirdPos.x = PositionToProgress(_birdPosition);
+		GUI.DrawTexture(_progressBirdPos, birdTexture);
+		_progressPlayerPos.x = PositionToProgress(_playerPosition);
+		GUI.DrawTexture(_progressPlayerPos, playerTexture);
+		
 		if(_gameOver)
 		{
 			if(_winner == Winner.Bird)
