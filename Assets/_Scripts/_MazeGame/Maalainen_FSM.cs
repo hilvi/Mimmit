857601b11d9028602e5a141a4ee6898e4a85cdeb
Scott <scjlaw@gmail.com>
2013-11-11 13:21:08 +0200

    added dupes of level one and worked on script AI

---
+++ Assets/_Scripts/_MazeGame/Maalainen_FSM.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maalainen_FSM : WayPointScript{
+	public enum maa_States {
+		Wander,
+		Guide,
+		Idle,
+		Return,
+	}
+	public Maalainen_FSM curState;
+	public float curSpeed;
+	public float curRotSpeed;
+	bool hungry;
+	// Use this for initialization
+	void Start () {
+	curState=Wander;
+	hungry=true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/_MazeGame/WayPointScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPointScript : MonoBehaviour {
+	public Transform _transform;
+	Vector3 destPos;
+	public GameObject[] WayPoints;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
