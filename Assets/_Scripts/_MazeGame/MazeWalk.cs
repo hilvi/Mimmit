d93fbeddff5de83f7387cf3daaf4714e8d09a4a2
unknown <scottl@VW-VISUAL-16.pk.amk.root>
2013-11-13 12:02:16 +0200

    deleted script and swapped with a simpler idea

---
+++ Assets/_Scripts/_MazeGame/MazeWalk.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script is for the movement done solely by the computer keys to allow looking and moving forward
+
+public class MazeWalk : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))transform.Translate(Vector3.forward*Time.deltaTime);
+		if(Input.GetKey(KeyCode.DownArrow))transform.Translate(-Vector3.forward*Time.deltaTime);
+		if(Input.GetKey(KeyCode.RightArrow))transform.Rotate(0,1,0);
+		if(Input.GetKey(KeyCode.LeftArrow))transform.Rotate(0,-1,0);
+	}
+}
