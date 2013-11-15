081f2d6b0b6f6a598ee4ce03657e4e0e0d7fcd9c
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-15 18:37:24 +0200

    Implement mouse grab mechanics

---
+++ Assets/FoodObjectScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodObjectScript : MonoBehaviour
+{
+    void Start ()
+    {
+        renderer.material.color = Color.red;
+    }
+	
+    void Update ()
+    {
+	
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.name == "Preparing Table") {
+            Debug.Log ("prep table");
+        }
+    }
+}
