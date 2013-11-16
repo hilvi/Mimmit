afcc1b63f521b2b87343e74c76c18469675b755f
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-16 18:38:47 +0200

    Slide food to prep table when close

--- Assets/_Scripts/_FoodGame/PreparationTableScript.cs
+++ Assets/_Scripts/_FoodGame/PreparationTableScript.cs
@@ -3,19 +3,6 @@ using System.Collections;
 
 public class PreparationTableScript : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start ()
-    {
-	
-    }
-	
-    // Update is called once per frame
-    void Update ()
-    {
-	
-    }
-
     void OnTriggerEnter (Collider col)
     {
         if (col.CompareTag ("FoodObject")) {
@@ -27,6 +14,24 @@ public class PreparationTableScript : MonoBehaviour
                 Debug.LogError ("FoodObject is missing script");
 
             Debug.Log ("Food came in");
+            StartCoroutine(SnapFoodToTable(col.gameObject));
+        }
+    }
+
+    private IEnumerator SnapFoodToTable(GameObject o) {
+
+        // Ignore z
+        Vector3 prepTablePos = new Vector3(transform.position.x, transform.position.y, 0f);
+        Vector3 foodPos = o.transform.position;
+        while (true)
+        {
+            foodPos = Vector3.MoveTowards(foodPos, prepTablePos, Time.deltaTime * 5f);
+            if (foodPos == prepTablePos)
+                break;
+
+            o.transform.position = foodPos;
+
+            yield return null;
         }
     }
 }
