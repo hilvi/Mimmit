3dddb07d55beb9707e10beec48c4e824f0385694
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-22 22:19:33 +0200

    Experiment with Unity2D stuff

---
+++ Assets/_Scripts/_PandaGame/SlopeTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlopeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var edgeCol = GetComponent<EdgeCollider2D>();
+
+        Vector2[] set = new Vector2[10];
+        for (int i = 0; i < set.Length; i++)
+        {
+            set[i] = new Vector2(i, Random.value * 2f);
+        }
+
+        edgeCol.points = set;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
