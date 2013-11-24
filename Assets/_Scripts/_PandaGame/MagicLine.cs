53c83d1c86922df731ca668bfccab6e1f5002cc1
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-25 00:11:20 +0200

    Support for multiple lines

---
+++ Assets/_Scripts/_PandaGame/MagicLine.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MagicLine : MonoBehaviour
+{
+    #region MEMBERS
+    private int samples = 0;
+    private EdgeCollider2D edgeCollider;
+    private LineRenderer lineRenderer;
+    #endregion
+
+    #region UNITY_METHODS
+    void Awake()
+    {
+        // Set references
+        edgeCollider = GetComponent<EdgeCollider2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetVertexCount(0);
+
+        // Reset whatever points edge collider might have
+        edgeCollider.points = new Vector2[2];
+    }
+    #endregion
+
+    #region METHODS
+    public void Step()
+    {
+        // Get mouse position and convert to world pos
+        Vector3 __pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        __pos.z = 0f; // Ditch z-axis, because line renderer only accepts Vector3
+        samples++; // Count samples for indexing
+
+        Vector2[] __oldPointSet = edgeCollider.points;
+        if (samples == 1 || samples == 2)
+        {
+            // Special case, because edge collider always have at least two points
+            __oldPointSet[samples - 1] = new Vector2(__pos.x, __pos.y);
+            edgeCollider.points = __oldPointSet;
+
+            // Update line renderer
+            lineRenderer.SetVertexCount(samples);
+            lineRenderer.SetPosition(samples - 1, __pos);
+        }
+        else
+        {
+            // Create new array of size points + 1
+            Vector2[] __newPointSet = new Vector2[__oldPointSet.Length + 1];
+            for (int i = 0; i < __oldPointSet.Length; i++)
+                __newPointSet[i] = __oldPointSet[i];
+
+            // Assign new point and save new point set to edge collider
+            __newPointSet[__oldPointSet.Length] = new Vector2(__pos.x, __pos.y);
+            edgeCollider.points = __newPointSet;
+
+            // Update line renderer
+            lineRenderer.SetVertexCount(samples);
+            lineRenderer.SetPosition(samples - 1, __pos);
+        }
+    }
+    #endregion
+}
--- Assets/_Scripts/_PandaGame/PandaGameManager.cs
+++ Assets/_Scripts/_PandaGame/PandaGameManager.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PandaGameManager : GameManager
 {
+    #region MEMBERS
+    // Boilerplate
     public GameObject musicObject;
     public AudioClip music;
+    // Line Drawing 
+    public GameObject linePrefab;
+    private bool _dragging;
+    private MagicLine _line;
+    private GameObject _lineContainer; // Store lines, keep hierarchy clean
+    #endregion
 
-	// Use this for initialization
+    #region UNITY_METHODS
     public override void Start()
     {
+        // Boilerplate
         base.Start();
         SetGameState(GameState.Running);
 
@@ -19,10 +29,34 @@ public class PandaGameManager : GameManager
             InGameMenuGUI.music.audio.Play();
             InGameMenuGUI.music.audio.loop = true;
         }
+
+        _lineContainer = new GameObject("Line Container");
     }
 	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+        // Left mouse down - Start drawing
+        if (Input.GetMouseButtonDown(0))
+        {
+            _dragging = true;
+
+            // Preserve active line, because GetComponent is relatively expensive 
+            var __o = (GameObject)(Instantiate(linePrefab));
+            __o.transform.parent = _lineContainer.transform;
+            _line = __o.GetComponent<MagicLine>();
+        }
+
+        //  Left mouse up  - Stop drawing
+        if (Input.GetMouseButtonUp(0))
+        {
+            _dragging = false;
+            _line = null;
+        }
+
+        // If currently drawing, advance magic line by one frame
+        if (_dragging && _line != null)
+        {
+            _line.Step();
+        }
+    }
+    #endregion
 }
