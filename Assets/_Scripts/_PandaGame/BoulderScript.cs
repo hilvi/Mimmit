8cee738eb2da05796fe42a5afb012e25d786e2c3
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-30 12:18:29 +0200

    Add falling boulder

---
+++ Assets/_Scripts/_PandaGame/BoulderScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderScript : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+
+    #region UNITY_METHODS
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnEnable()
+    {
+        PandaBallScript.OnBallActivate += DropStone;
+    }
+
+    void OnDisable()
+    {
+        PandaBallScript.OnBallActivate -= DropStone;
+    }
+    #endregion
+
+    private void DropStone()
+    {
+        _rigidBody.isKinematic = false;
+    }
+}
