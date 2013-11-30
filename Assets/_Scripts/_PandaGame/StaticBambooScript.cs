2af2961bc03e2628247c5c7216ec89e826304fc6
Vinh Truong <vinh.truong@metropolia.fi>
2013-11-30 13:05:02 +0200

    No need to continuously update static bamboo

--- Assets/_Scripts/_PandaGame/StaticBambooScript.cs
+++ Assets/_Scripts/_PandaGame/StaticBambooScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class StaticBambooScript : MonoBehaviour
 {
-    void Update()
+    void Start()
     {
         // Set texture tiling proportional to its scale
         Vector2 __newTiling = new Vector2(transform.localScale.x, 1f);
