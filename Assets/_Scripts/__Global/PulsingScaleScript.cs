8640a6b5b0ea28b17909c2b427e8d19fbc4d68b1
Vinh Truong <vinh.truong@metropolia.fi>
2013-12-04 17:02:30 +0200

    Improvements to powerup
    - Leafs disappear when panda rolls over it
    - Powerups have a pulsing glow

---
+++ Assets/_Scripts/__Global/PulsingScaleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulsingScaleScript : MonoBehaviour {
+
+	public float pulsingRate;
+	public float pulsingSize;
+
+	private Vector3 _defaultScale;
+
+	void Awake() {
+		_defaultScale = transform.localScale;
+	}
+
+	void Update () {
+		// y = 0.5 + 0.5 * cos(t) oscillates between 0 and 1
+		float __multiplier = 0.5f + 0.5f * Mathf.Cos (Time.time * pulsingRate);
+
+		Vector3 __newScale = new Vector3();
+		__newScale.x = _defaultScale.x + pulsingSize * __multiplier * _defaultScale.x;
+		__newScale.y = _defaultScale.y + pulsingSize * __multiplier * _defaultScale.y;
+
+		transform.localScale = __newScale;
+	}
+}
