a9d38f285c6c58fc1c21963fa3d022fb0e84d8de
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-12-14 14:46:54 +0200

    Make item movement seem more natural

--- Assets/_Scripts/_GrabGame/HarakkaItemScript.cs
+++ Assets/_Scripts/_GrabGame/HarakkaItemScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class HarakkaItemScript : MonoBehaviour {
 	public GameObject fallingObject;
 	public bool moving = false;
+	public float velocity = 200;
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +13,10 @@ public class HarakkaItemScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(moving)
-			transform.position += new Vector3(0, 200*Time.deltaTime, 0);
+		if(moving) {
+			transform.position += new Vector3(0, velocity*Time.deltaTime, 0);
+			velocity -= 9.81f;
+		}
 	}
 
 	void OnBecameInvisible() {
