5fabeb7596d59f0c1a3891115778fd2ab0710226
Vinh Truong <vinh.truong@metropolia.fi>
2013-12-18 17:39:30 +0200

    Panda level re-design
    - Instead of bamboo, use polygon colliders with level bounds texture
    - Add rotating obstacle

---
+++ Assets/_Scripts/_PandaGame/RotatorScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorScript : MonoBehaviour
+{
+    #region MEMBERS
+    public float rotateSpeed;
+    public float angularVelocityLimit;
+    #endregion
+
+    #region UNITY_METHODS
+    void FixedUpdate()
+    {
+        rigidbody2D.AddTorque(rotateSpeed);
+        if (rigidbody2D.angularVelocity > angularVelocityLimit)
+            rigidbody2D.angularVelocity = angularVelocityLimit;
+    }
+    #endregion
+}
