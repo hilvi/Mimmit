e35f713284296de453a62f3f25e662fd0125a8d6
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-03-23 14:56:49 +0200

    Lots of important changes

--- Assets/_Scripts/_Utility/BackgroundScript.cs
+++ Assets/_Scripts/_Utility/BackgroundScript.cs
@@ -8,8 +8,8 @@ public class BackgroundScript : MonoBehaviour {
 	{
 		background = GetComponent<GUITexture>();
 		// Setting background to full screen
-		float width = 960;
-        float height = 600;
+		float width = Screen.width;
+        float height = Screen.height;
         Rect rect = new Rect(-width / 2, -height / 2, width, height);
 		background.pixelInset = rect;
 	}
